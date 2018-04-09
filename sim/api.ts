285e96bab710bfec46c5520aa222f0476e7e8b6a
agendreau <alex.gendreau24@gmail.com>
2018-04-09 16:08:31 -0600

    updated for sensor system

--- sim/api.ts
+++ sim/api.ts
@@ -2,25 +2,18 @@
 
 //import * as WebRequest from 'web-request';
 
+var sensor = '192.168.4.63';
 
-namespace pxsim.sensors {
+
+//namespace pxsim.sensorsStore {
     //import axios from '../node_modules/axios';
     //var request = require('web-request')
     
 
     //% weight = 90
     //% blockId=tempF block="Temperature"
-    export function tempF(){
-        //board().tempF.y+=
-        board().updateView();
-
-    }
-     //% weight = 85
-    //% blockId=humid block="Humidity"
-    export async function humidityAsync(){
-
-        
-            const response = await fetch('http://api.wunderground.com/api/8f5846f4c43e4050/conditions/q/CO/Denver.json')
+    //export async function tempFAsync(){
+        /*const response = await fetch('http://api.wunderground.com/api/8f5846f4c43e4050/conditions/q/CO/Denver.json')
         
             const json = await response.json();
         
@@ -29,13 +22,33 @@ namespace pxsim.sensors {
             var h =  parseFloat(json.current_observation.temp_f);
             console.log("in update function");
             //var svg = d3.select("humid").transform.attr("cy",50);
+            d3.select("#temp_text").text(h);
+            d3.select("#temp_text").attr("y",200-h);
+            d3.select("#temp").attr("cy",200-h);
+            console.log("past update");*/
+
+    //}
+     //% weight = 85
+    //% blockId=humid block="Humidity"
+    //export async function humidityAsync(){
+
+        
+            /*const response = await fetch('http://api.wunderground.com/api/8f5846f4c43e4050/conditions/q/CO/Denver.json')
+        
+            const json = await response.json();
+        
+            console.log(json.current_observation.relative_humidity.replace('%',''));
+            //board().updateView();
+            var h =  parseFloat(json.current_observation.relative_humidity.replace('%',''));
+            console.log("in update function");
+            //var svg = d3.select("humid").transform.attr("cy",50);
             d3.select("#humid_text").text(h);
             d3.select("#humid_text").attr("y",200-h);
             d3.select("#humid").attr("cy",200-h);
-            console.log("past update");
+            console.log("past update");*/
            
             
-        }
+        //}
        
        /* fetch('http://www.google.com/').then(function(response) {
             console.log(stringify(response)})*/
@@ -46,48 +59,234 @@ namespace pxsim.sensors {
 
      //% weight = 80
     //% blockId=pressure block="Pressure"
-    export function pressure(){
+   // export async function pressureAsync(){
+        /*const response = await fetch('http://api.wunderground.com/api/8f5846f4c43e4050/conditions/q/CO/Denver.json')
+        
+            const json = await response.json();
+        
+            console.log(json.current_observation.pressure_in);
+            //board().updateView();
+            var h =  parseFloat(json.current_observation.pressure_in);
+            console.log("in update function");
+            //var svg = d3.select("humid").transform.attr("cy",50);
+            d3.select("#pressure_text").text(h);
+            d3.select("#pressure_text").attr("y",200-h);
+            d3.select("#pressure").attr("cy",200-h);
+            console.log("past update");*/
 
-    }
+    //}
+
+       //% weight = 75
+    //% blockId=pressure block="Pressure"
+    //export async function altitudeAsync(){
+        /*const response = await fetch('http://api.wunderground.com/api/8f5846f4c43e4050/conditions/q/CO/Denver.json')
+        
+            const json = await response.json();
+        
+            console.log(json.observation_location.elevation);
+            //board().updateView();
+            var h =  parseFloat(json.observation_location.relative_humidity);
+            console.log("in update function");
+            //var svg = d3.select("humid").transform.attr("cy",50);
+            d3.select("#altitude_text").text(h);
+            d3.select("#altitude_text").attr("y",200-h);
+            d3.select("#altitude").attr("cy",200-h);
+            console.log("past update");*/
+   // }
 
      //% weight = 75
     //% blockId=tvoc block="TVOC"
-    export function tvoc(){
+    /*export async function tvocAsync(){
+        const response = await fetch('https://api.wunderground.com/api/8f5846f4c43e4050/conditions/q/CO/Denver.json')
+        
+            const json = await response.json();
+        
+            console.log(json.current_observation.relative_humidity);
+            //board().updateView();
+            var h =  parseFloat(json.current_observation.relative_humidity);
+            console.log("in update function");
+            //var svg = d3.select("humid").transform.attr("cy",50);
+            d3.select("#humid_text").text(h);
+            d3.select("#humid_text").attr("y",200-h);
+            d3.select("#humid").attr("cy",200-h);
+            console.log("past update");
 
-    }
+    }*/
 
      //% weight = 70
     //% blockId=co2 block="CO2"
-    export function co2(){
+    /*export async function co2Async(){
+        const response = await fetch('https://api.wunderground.com/api/8f5846f4c43e4050/conditions/q/CO/Denver.json')
+        
+            const json = await response.json();
+        
+            console.log(json.current_observation.relative_humidity);
+            //board().updateView();
+            var h =  parseFloat(json.current_observation.relative_humidity);
+            console.log("in update function");
+            //var svg = d3.select("humid").transform.attr("cy",50);
+            d3.select("#humid_text").text(h);
+            d3.select("#humid_text").attr("y",200-h);
+            d3.select("#humid").attr("cy",200-h);
+            console.log("past update");
 
+    }*/
+//}
+
+namespace pxsim.Sensors {
+
+    
+    //% weight = 90
+    //% blockId=humidity1 block="Humidity"
+    export async function humidityAsync(){
+        console.log(sensor)
+        //const response = await fetch('https://api.wunderground.com/api/'+sensor+'/conditions/q/CO/Denver.json')
+        const response = await fetch(sensor+'/dev/sensors?sensor=bme280&sample=humidity')
+
+            const json = await response.json();
+            
+            console.log(json.value);
+            //console.log(json.current_observation.relative_humidity.replace(re,''));
+            //board().updateView();
+            var h =  parseFloat(json.value);
+            console.log("in update function");
+            //var svg = d3.select("humid").transform.attr("cy",50);
+            d3.select("#humid_text").text("Humid: \n" + h);
+            /*d3.select("#humid_text").attr("y",200-h);
+            d3.select("#humid").attr("cy",200-h);*/
+            console.log("past update");
+
+            return h
     }
+
+    //% weight = 85
+    //% blockId=tempF1 block="Temperature"
+    export async function tempFAsync(){
+        console.log(sensor);
+           //const response = await fetch('https://api.wunderground.com/api/'+sensor+'/conditions/q/CO/Denver.json')
+           const response = await fetch(sensor+'/dev/sensors?sensor=bme280&sample=temperaturef')
+
+            const json = await response.json();
+        
+            console.log(json.value);
+            //board().updateView();
+            var t =  parseFloat(json.value);
+            console.log("in update function");
+            //var svg = d3.select("humid").transform.attr("cy",50);
+            d3.select("#temp_text").text("Temp: \n" + t);
+            /*d3.select("#temp_text").attr("y",200-t);
+            d3.select("#temp").attr("cy",200-t);*/
+            console.log("past update");
+
+            return t;
+    }
+
+    //% weight = 80
+    //% blockId=pressure1 block="Pressure"
+    /*export function pressure(){
+        return 1;
+    }*/
 }
 
 namespace pxsim.SensorSystem {
+  
+    /*enum SenSys {
+        
+        "Panda" = 1,
+        
+        "Tiger" = 2,
+        "Dolphin" = 3,
+        "Gorilla" = 4,
+    
+      };*/
+
+ 
+
     //% weight = 90
-    //% blockId=connect block="Sensor System %Name"
-    export function connect(name: string){
+    //% blockId=connect block="Sensor System %name"
+
+    export function download(filename:string, text:string) {
+        var element = document.createElement('a');
+        element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+        element.setAttribute('download', filename);
+    
+        element.style.display = 'none';
+        document.body.appendChild(element);
+    
+        element.click();
+    
+        document.body.removeChild(element);
+    }
+  
+    
+    
+    export function connect(name: SenSys){
+        switch(name) {
+        case SenSys.Panda:
+            //sensor = "02fb20304a73bce1";
+            sensor = '192.168.4.63';
+            break;
+        case SenSys.Tiger:
+            //sensor = "434d63a333987665";
+            sensor = '192.168.4.63';
+            break;
+        case SenSys.Dolphin: 
+            //sensor = "fc51f1c83be0df29";
+            sensor = '192.168.4.63';
+            break;
+       case SenSys.Gorilla:
+            //sensor = "b379867083543cc0";
+            sensor = '192.168.4.63';
+            break;
+        }
+        
+    
 
     }
 
     //% weight = 85
-    //% blockId=sample_rate block="Sample Rate %Rate"
-    export function sample_rate(rate: number){
+    //% blockId=sample_rate block="Sample Rate %Rate (sec)"
+    /*export function sample_rate(rate: number){
 
-    }
+    }*/
 
     //% weight = 85
     //% blockId=upload block="Upload %filename"
-    export function upload(filename: string){
+    export async function uploadAsync(filename: string){
+        await fetch('192.168.4.63/datalog.csv')
 
     }
+
+    //% weight = 84
+    //% blockId=dataFile block="DataFile %filename"
+    /*export function dataFile(filename: string){
+
+    }*/
+
+
+
+
+    //% weight 80
+    //% blockId=totalSamples block="Total Samples %total"
+    /*export function totalSamples(total: number){
+
+    }*/
+
+      //% weight 75
+    //% blockId=writeValue block="Store Value %total"
+    export function writeValue(reading: number){
+
+    }
+
+
+
+    
 } 
 
-namespace pxsim.Data {
+/*namespace pxsim.Data {
     
-    //%blockId="led" block="light LED %rgb=colorNumberPicker"
-    //% hop.fieldEditor="gridPicker"
-    export function LightLED(rgb: number){
+    //%blockId="led" block="light LED %color"
+    export function lightLED(color: string){
 
     }
 
@@ -95,25 +294,25 @@ namespace pxsim.Data {
      export function makeNoise(howLoud: number){
 
     }
-}
+}*/
 
 
-namespace pxsim.hare {
+//namespace pxsim.hare {
     /**
      * This is hop
      */
     //% blockId="sampleHop" block="hop %hop on color %color=colorNumberPicker"
     //% hop.fieldEditor="gridPicker"
-    export function hop(hop: Hop, color: number) {
+    /*export function hop(hop: Hop, color: number) {
 
-    }
+    }*/
 
     //% blockId=sampleOnLand block="on land"
     //% optionalVariableArgs
-    export function onLand(handler: (height: number, more: number, most: number) => void) {
+    /*export function onLand(handler: (height: number, more: number, most: number) => void) {
 
-    }
-}
+    }*/
+//}
 
 //namespace pxsim.turtle {
     /**
@@ -164,9 +363,9 @@ namespace pxsim.loops {
      */
     //% help=functions/forever weight=55 blockGap=8
     //% blockId=device_forever block="forever" 
-    export function forever(body: RefAction): void {
+    /*export function forever(body: RefAction): void {
         thread.forever(body)
-    }
+    }*/
 
     /**
      * Pause for the specified time in milliseconds
@@ -210,7 +409,7 @@ namespace pxsim {
 
 
 
-    export class Sprite {
+    //export class Sprite {
 
         //import * as WebRequest from '../node_modules/web-request';
 
@@ -220,18 +419,18 @@ namespace pxsim {
          * The X-coordiante
          */
         //%
-        public x = 100;
+        //public x = 100;
          /**
          * The Y-coordiante
          */
         //%
-        public y = 100;
-        public angle = 90;
+        //public y = 100;
+        //public angle = 90;
         
         /*constructor() {
         }*/
 
-        constructor(new_x:number,new_y:number){
+        /*constructor(new_x:number,new_y:number){
             this.x=new_x
             this.y=new_y
             this.angle=90
@@ -239,7 +438,7 @@ namespace pxsim {
         
         private foobar() {}
 
-        public gettemp(){
+        public gettemp(){*/
             
             
             
@@ -262,13 +461,13 @@ namespace pxsim {
  
             //return Promise.delay(400)
         
-        }
+        //}
 
         /**
          * Move the thing forward
          */
         //%
-        public forwardAsync(steps: number) {
+        /*public forwardAsync(steps: number) {
 
             //var result = await WebRequest.get('http://www.google.com/');
             //console.log(result.content);
@@ -282,15 +481,15 @@ namespace pxsim {
 
             return Promise.delay(400)
         }
-    }
+    }*/
 }
 
-namespace pxsim.sprites {
+/*namespace pxsim.sprites {
     /**
      * Creates a new sprite
      */
     //% blockId="sampleCreate" block="createSprite"
-    export function createSprite(pos_x:number,pos_y:number): Sprite {
+    /*export function createSprite(pos_x:number,pos_y:number): Sprite {
         return new Sprite(pos_x,pos_y);
     }
-}
+}*/
